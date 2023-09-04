345d6cf53d2e7f61876e6ae7f623178afc8e82ca
Guilherme Mattos <91735703+Gmattos14@users.noreply.github.com>
2023-09-04 17:44:59 -0300

    Tradução adicionada na página de login
    Adicionada a tradução PT-BR para a página de login

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Esqueceu a senha? Sem problemas. Digite o seu email abaixo para que possamos enviar um link para trocar a senha.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Enviar o link de redefinição') }}
             </x-primary-button>
         </div>
     </form>
